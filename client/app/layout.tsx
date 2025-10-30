c55bdd39e341d69bcae8403c281e6db2c7a7fac4
Kelvin <kelvinmurithi@live.com>
2025-10-31 02:33:33 +0300

    Add Microsoft clarity

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -39,6 +39,17 @@ export default function RootLayout({
             gtag('config', 'G-LJX330RQP4');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tyi54nkzv2");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
